772a4f0facf222083507d6d7a6a137b245aac01a
Raymon Epping <your@email.com>
2025-07-09 18:29:34 +0200

    Add Vault Radar demo leak file

---
+++ radar_demo/Vault_Radar_trigger.tf
@@ -0,0 +1,15 @@
+###############################################################
+# Vault Radar Demo Leak Seed
+# Version: 2.1.15
+# Timestamp: 2025-07-09 18:29:33
+# Run ID: 1752078573-13501
+# Author: raymon.epping
+# Scenario: AWS
+###############################################################
+# [secret][high] AWS Access Key
+variable "AWS_Access_Key" { default = "AWS_ACCESS_KEY_ID=AKIA1234567890FAKE" }
+
+###############################################################
+# End of Vault Radar Demo Leak Seed
+# For educational/demo use only!
+###############################################################
